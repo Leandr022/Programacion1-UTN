2f9781aaf10d495e9511594fbca199aeb02f2fbc
Leandr022 <leandro.santagada@gmail.com>
2025-04-10 21:22:26 -0300

    realizando el tp4

---
+++ 04 Estructuras Repetitivas/Tp4-Estructuras-Repetitivas-Santagada.py
@@ -0,0 +1,11 @@
+# Programa para imprimir números enteros de 0 a 100
+
+# Utilizamos un bucle FOR para iterar a través de la secuencia de números.
+# La función range(101) genera una secuencia de números desde 0 hasta 100
+# (el límite superior no se incluye, por eso ponemos 101).
+for numero in range(101):
+    # La función print() se utiliza para mostrar el valor de la variable 'numero'
+    # en la pantalla. Cada llamada a print() genera una nueva línea.
+    print(numero)
+
+# Fin del programa
